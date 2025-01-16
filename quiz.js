52072e9239b049c6d9acbe18e4ff532d843e1a78
nato1212 <natananamsalu@gmail.com>
2025-01-16 09:03:40 +0300

    simple quiz completed

--- quiz.js
+++ quiz.js
@@ -1,15 +1,17 @@
 function checkAnswer() {
   const correctAnswer = "4";
-  const checkedAnswer = document.querySelector(
+  const userAnswer = document.querySelector(
     'input[name="quiz"]:checked'
   )?.value;
-  const feedback = document.getElementById("feedback");
-  if (checkedAnswer === correctAnswer) {
-    feedback.textContent = "Correct! Well done.";
+  const feedbackElement = document.getElementById("feedback");
+  if (userAnswer === correctAnswer) {
+    feedbackElement.textContent = "Correct! Well done.";
+    feedbackElement.style.color = "green";
   } else {
-    feedback.textContent = "That's incorrect. Try again!";
+    feedbackElement.textContent = "That's incorrect. Try again!";
+    feedbackElement.style.color = "red";
   }
 }
 
-const submit_answer = document.getElementById("submit-answer");
-submit_answer.addEventListener("click", checkAnswer);
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
