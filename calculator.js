0e472d2eedbf19b65e815b08d03828f54a56254f
nato1212 <natananamsalu@gmail.com>
2025-01-16 09:09:54 +0300

    simple calculator completed

---
+++ calculator.js
@@ -0,0 +1,42 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  if (number2 === 0) {
+    return "Error: Division by zero";
+  }
+  return number1 / number2;
+}
+
+document.getElementById("add").addEventListener("click", function () {
+  calculate(add);
+});
+
+document.getElementById("subtract").addEventListener("click", function () {
+  calculate(subtract);
+});
+
+document.getElementById("multiply").addEventListener("click", function () {
+  calculate(multiply);
+});
+
+document.getElementById("divide").addEventListener("click", function () {
+  calculate(divide);
+});
+
+function calculate(operation) {
+  const number1 = parseFloat(document.getElementById("number1").value) || 0;
+  const number2 = parseFloat(document.getElementById("number2").value) || 0;
+
+  const result = operation(number1, number2);
+  document.getElementById("calculation-result").textContent = result;
+}
